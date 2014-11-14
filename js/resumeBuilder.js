b36e212f5ab092b642137a1fb35fcc427d46a56a
Tyler Damon <damonsolutions@gmail.com>
2014-11-14 11:18:42 +0800

    JS Basics 1.2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,3 +4,7 @@ var formattedName = HTMLheaderName.replace("%data%", "Tyler Damon");
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+
+var skills = ["Programming"," Chinese"," International Business"];
+$("#main").append(skills);
